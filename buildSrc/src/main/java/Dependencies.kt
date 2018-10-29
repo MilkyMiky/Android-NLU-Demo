e844578e984df17f9efffca7ae1405bbfb3eed83
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-10-29 19:09:27 +0300

    Dependencies

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,16 @@
+
+object Versions {
+    // project
+    const val compileSdkVersion = 28
+    const val applicationId = "com.example.user.samplenlu"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+    const val versionCode = 1
+    const val  versionName = "1.0"
+}
+
+
+object Deps{
+
+
+}
